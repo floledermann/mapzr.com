02446d3263e466179ec60326858b6aea753cce40
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2019-02-17 12:05:13 +0100

    add some SVG icons

--- mapzr.js
+++ mapzr.js
@@ -99,7 +99,7 @@ function createPopup() {
   let editForm = document.createElement("DIV");
   editForm.innerHTML = '<input type="text" class="field-title" size="20" placeholder="Title">' +
                 '<textarea class="field-description" autocomplete="off" placeholder="Description" style="resize: none;"></textarea>' +
-                '<small>Lat/Lon: 0, 0 (copy)</small>' +
+                '<small>Lat/Lon: <span class="latlng">0, 0</span> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="with: 12px; height: 12px; vertical-align:-2px;"><path d="M296 48H176.5C154.4 48 136 65.4 136 87.5V96h-7.5C106.4 96 88 113.4 88 135.5v288c0 22.1 18.4 40.5 40.5 40.5h208c22.1 0 39.5-18.4 39.5-40.5V416h8.5c22.1 0 39.5-18.4 39.5-40.5V176L296 48zm0 44.6l83.4 83.4H296V92.6zm48 330.9c0 4.7-3.4 8.5-7.5 8.5h-208c-4.4 0-8.5-4.1-8.5-8.5v-288c0-4.1 3.8-7.5 8.5-7.5h7.5v255.5c0 22.1 10.4 32.5 32.5 32.5H344v7.5zm48-48c0 4.7-3.4 8.5-7.5 8.5h-208c-4.4 0-8.5-4.1-8.5-8.5v-288c0-4.1 3.8-7.5 8.5-7.5H264v128h128v167.5z"/></svg></small>' +
                 '<div><button class="button-ok">OK</button> <button class="button-cancel">Cancel</button></div>';
     
   let editPopup = L.popup({
